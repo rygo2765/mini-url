5ede125c176106f48e145e57b997ed227fa905e3
Ryan Goh <ryangohce@gmail.com>
2024-10-15 07:19:27 +0000

    Added URL title fetch method with error handling

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,6 @@
+require "nokogiri"
+require "open-uri"
+
 class Url < ApplicationRecord
   # Unique ID Length determines the number of characters in the Short URL path
   UNIQUE_ID_LENGTH = 8
@@ -5,7 +8,7 @@ class Url < ApplicationRecord
   # Validation to ensure the target URL is in the correct format
   validates :target_url, presence: true, format: { with: URI.regexp(%w[http https]), message: "must be a valid URL" }
 
-  before_create :generate_short_url
+  before_create :generate_short_url, :fetch_title_from_target_url
 
   private
 
@@ -14,5 +17,19 @@ class Url < ApplicationRecord
     # Generate a random short code based on Unique Id Length
     short_url_path = SecureRandom.base58(UNIQUE_ID_LENGTH)
     self.short_url = "https://miniurl.com/#{short_url_path}"
+  end
+
+  # Method to fetch title form the target URL
+  def fetch_title_from_target_url
+    begin
+      doc = Nokogiri::HTML(URI.parse(target_url).open)
+      self.title = doc.title if doc
+    rescue OpenURI::HTTPError => e
+      Rails.logger.error "HTTP error fetching title from URL: #{e.message}"
+      self.title = "Unknown Title"
+    rescue => e
+      Rails.logger.error "Failed to fetch title from URL: #{e.message}"
+      self.title = "Unknown Title"
+    end
   end
 end
