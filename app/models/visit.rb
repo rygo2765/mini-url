086cefdc0c525eab0ef5e47c40b03d0246e2f05e
Ryan Goh <ryangohce@gmail.com>
2024-10-17 05:51:11 +0000

    Implement error handling for geolocation tracking and add region column

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -9,10 +9,16 @@ class Visit < ApplicationRecord
   def geocode_ip_address
     return unless ip_address.present?
 
-    location_data = Geocoder.search(ip_address).first
-    if location_data
-      self.city = location_data.city
-      self.country = location_data.country
+    begin
+      location_data = Geocoder.search(ip_address).first
+      self.city = location_data.city || "Unknown"
+      self.region = location_data.region || "Unknown"
+      self.country = location_data.country || "Unknown"
+    rescue => e
+      Rails.logger.error "Geocoding error for IP #{ip_address}: #{e.message}"
+      self.city = "Unknown"
+      self.region = "Unknown"
+      self.country = "Unknown"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_051732) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_17_054529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_051732) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "region"
     t.index ["url_id"], name: "index_visits_on_url_id"
   end
 
