87cec998b14b82a690b62137392cbf628be2f5e5
Ryan Goh <ryangohce@gmail.com>
2024-10-21 12:38:13 +0000

    updated unit test for Url model

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Url, type: :model do
   let(:valid_url) { 'https://example.com' }
   let(:invalid_url) { 'invalid-url' }
+  let(:user_uuid) { '123e4567-e89b-12d3-a456-426614174000' }
 
   before do
     stub_request(:get, "https://example.com/")
@@ -13,34 +14,42 @@ RSpec.describe Url, type: :model do
   describe 'validations' do
     context 'with a valid target_url' do
       it 'is valid' do
-        url = Url.new(target_url: valid_url)
+        url = Url.new(target_url: valid_url, user_uuid: user_uuid)
         expect(url).to be_valid
       end
     end
 
     context 'without a target_url' do
       it 'is invalid' do
-        url = Url.new(target_url: '')
+        url = Url.new(target_url: '', user_uuid: user_uuid)
         expect(url).to_not be_valid
       end
     end
 
     context 'with an improperly formatted URL' do
       it 'is invalid' do
-        url = Url.new(target_url: invalid_url)
+        url = Url.new(target_url: invalid_url, user_uuid: user_uuid)
         expect(url).to_not be_valid
       end
     end
   end
 
+  context 'without a user_uuid' do
+    it 'is invalid without a user_uuid' do
+      url = Url.new(target_url: valid_url, user_uuid: nil)
+      expect(url).to_not be_valid
+    end
+  end
+
+
   # Describe short URL generation
   describe '#generate_short_url' do
-    let(:url) { Url.create(target_url: valid_url) }
+    let(:url) { Url.create(target_url: valid_url, user_uuid: user_uuid) }
 
     it 'generates unique short URL paths' do
       generated_urls = []
       100.times do
-        url = Url.create(target_url: "https://example.com")
+        url = Url.create(target_url: "https://example.com", user_uuid: user_uuid)
         generated_urls << url.short_url
       end
 
@@ -53,8 +62,8 @@ RSpec.describe Url, type: :model do
     end
 
     it 'allows multiple short URLs for the same target URL' do
-      url1 = Url.create(target_url: valid_url)
-      url2 = Url.create(target_url: valid_url)
+      url1 = Url.create(target_url: valid_url, user_uuid: user_uuid)
+      url2 = Url.create(target_url: valid_url, user_uuid: user_uuid)
       expect(url1.short_url).not_to eq(url2.short_url)
     end
   end
@@ -62,13 +71,13 @@ RSpec.describe Url, type: :model do
   # Describe title fetching
   describe '#fetch_title_from_target_url' do
     it 'fetches the title from the target URL' do
-      url = Url.create(target_url: valid_url)
+      url = Url.create(target_url: valid_url, user_uuid: user_uuid)
       expect(url.title).to eq('Example Website')
     end
 
     it 'sets "Unknown Title" when unable to fetch title' do
       stub_request(:get, "https://example.com").to_return(status: 404)
-      url = Url.create(target_url: valid_url)
+      url = Url.create(target_url: valid_url, user_uuid: user_uuid)
       expect(url.title).to eq('Unknown Title')
     end
   end
@@ -76,13 +85,13 @@ RSpec.describe Url, type: :model do
   # Describe sanitize target_url
   describe '#sanitize_target_url' do
     it 'adds http:// to URLs without a scheme' do
-      url = Url.new(target_url: 'example.com')
+      url = Url.new(target_url: 'example.com', user_uuid: user_uuid)
       url.valid?
       expect(url.target_url).to eq('http://example.com')
     end
 
     it 'keeps https:// URLs unchanged' do
-      url = Url.new(target_url: valid_url)
+      url = Url.new(target_url: valid_url, user_uuid: user_uuid)
       url.valid?
       expect(url.target_url).to eq('https://example.com')
     end
